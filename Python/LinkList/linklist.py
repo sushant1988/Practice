0347b041a97c95aab36ac2efaaa199d597004593
Sushant Choudhary <sushantc@microsoft.com>
2021-03-05 23:47:40 -0800

    Initial Commit for linklist

---
+++ Python/LinkList/linklist.py
@@ -0,0 +1,52 @@
+class LinkListNode:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkList:
+    def __init__(self):
+        self.head = None
+        self.rear = None
+
+    def insert_at_front(self, data):
+        new_node = LinkListNode(data)
+        if self.head is None:
+            self.head = new_node
+            self.rear = self.head
+        else:
+            new_node.next = self.head
+            self.head = new_node
+    
+    def insert_at_rear(self, data):
+        new_node = LinkListNode(data)
+        if self.rear is None:
+            self.head = self.rear = new_node
+        else:
+            self.head.next = new_node
+
+    def traverse_linklist(self):
+        temp = self.head
+        while(temp is not None):
+            print(temp.data)
+            temp = temp.next
+
+    def find(self, data):
+        temp = self.head
+        while(temp is not None and temp.data != data):
+            temp = temp.next
+        
+        return temp
+
+
+if __name__== "__main__":
+    lk = LinkList()
+    lk.insert_at_front(2)
+    lk.insert_at_rear(3)
+    lk.insert_at_front(4)
+    lk.insert_at_front(5)
+    lk.traverse_linklist()
+
+    l = lk.find(3)
+    print(l.data)
+
+
