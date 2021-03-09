d436a418d3e631d4afafcf91774e2d6820b6499b
Sushant Choudhary <sushantmax88@gmail.com>
2021-03-09 10:27:20 -0800

    linklist test

--- Python/LinkList/linklist.py
+++ Python/LinkList/linklist.py
@@ -1,50 +1,144 @@
 class LinkListNode:
-    def __init__(self, data):
-        self.data = data
-        self.next = None
+  def __init__(self, data):
+      self.data = data
+      self.next = None
 
 class LinkList:
-    def __init__(self):
-        self.head = None
-        self.rear = None
+  def __init__(self):
+      self.head = None
+      self.rear = None
 
-    def insert_at_front(self, data):
-        new_node = LinkListNode(data)
-        if self.head is None:
-            self.head = new_node
-            self.rear = self.head
-        else:
-            new_node.next = self.head
-            self.head = new_node
+  @classmethod
+  def to_list(cls, linklist):
+    temp: LinkList = linklist.head
+    lst = []
+    while(temp is not None):
+      lst.append(temp.data)
+      temp = temp.next
 
-    def insert_at_rear(self, data):
-        new_node = LinkListNode(data)
-        if self.rear is None:
-            self.head = self.rear = new_node
-        else:
-            self.rear.next = new_node
-            self.rear = new_node
+    return lst
 
-    def traverse_linklist(self):
-        temp = self.head
-        while(temp is not None):
-            print(temp.data)
-            temp = temp.next
+  # reverse linklist
+  @classmethod
+  def reverse_recursive_linklist(cls, head):
+    if head.next is None:
+      return head
 
-    def find(self, data):
-        temp = self.head
-        while(temp is not None and temp.data != data):
-            temp = temp.next
+    new_head = LinkList.reverse_recursive_linklist(head.next)
+    head.next.next = head
+    head.next = None
 
-        return temp
+    return new_head
 
-    def return_list(self):
-      temp = self.head
-      lst = []
-      while(temp is not None):
-        lst.append(temp.data)
+  def insert_at_front(self, data):
+    new_node = LinkListNode(data)
+    if self.head is None:
+        self.head = new_node
+        self.rear = self.head
+    else:
+        new_node.next = self.head
+        self.head = new_node
+
+  def insert_at_rear(self, data):
+    new_node = LinkListNode(data)
+    if self.rear is None:
+        self.head = self.rear = new_node
+    else:
+        self.rear.next = new_node
+        self.rear = new_node
+
+  def print_linklist(self):
+    temp = self.head
+    print("[", end=" ")
+    while(temp is not None):
+        print(temp.data, end=" ")
         temp = temp.next
+    print("]")
 
-      return lst
+  def print_linklist(self):
+    temp = self.head
+    print("[", end=" ")
+    while(temp is not None):
+        print(temp.data, end=" ")
+        temp = temp.next
+    print("]")
+
+  def find(self, data):
+    temp = self.head
+    while(temp is not None and temp.data != data):
+      temp = temp.next
+
+    return temp
+
+  def to_list(self):
+    temp = self.head
+    lst = []
+    while(temp is not None):
+      lst.append(temp.data)
+      temp = temp.next
+
+    return lst
+
+  ''' Mth to Last Element in LinkList PIE Page 50
+      Time Complexity: O(N)
+      Space Complexity: O(1)
+  '''
+  def find_mth_from_last_element(self, M: int):
+    p1: LinkList = self.head
+    p2: LinkList = self.head
+
+    count = 0
+    while(p1 is not None and count < M):
+      p1 = p1.next
+      count += 1
+
+    if count < M:
+      return None
+
+    while(p1 is not None):
+      p2 = p2.next
+      p1 = p1.next
+
+    return p2.data
+
+  ''' Find Circular LinkList
+      Time Complexity: O(N)
+      Space Complexity: O(1) Constant
+  '''
+  def is_circular_list(self) -> bool:
+    if self.head is not None:
+      slow: LinkList = self.head
+      fast: LinkList = self.head
+
+      while(fast is not None and fast.next is not None):
+        if (slow == fast or fast.next == slow):
+          return True
+        slow = slow.next
+        fast = fast.next.next
+
+    return False
+
+  ''' Time Complexity = O(1)
+  '''
+  def reverse_iterative_linklist(self):
+    prev: LinkList = None
+    cur: LinkList = self.head
+    next: LinkList = self.head.next
+
+    while(next is not None):
+      cur.next = prev
+      prev = cur
+      cur = next
+      next = next.next
+
+    cur.next = prev
+    self.head = cur
+    pass
+
+
+
+
+
+
 
 
--- Python/LinkList/linklist_test.py
+++ Python/LinkList/linklist_test.py
@@ -2,30 +2,70 @@ import unittest
 from linklist import LinkList
 
 class LinkListTest(unittest.TestCase):
-    def setUp(self) -> LinkList:
-      return LinkList()
+  def setUp(self) -> LinkList:
+    return LinkList()
 
-    def test_fn_insert_at_front(self):
-        link: LinkList = self.setUp()
-        items: list = [2, 3, 4]
+  def test_fn_insert_at_front(self):
+    link: LinkList = self.setUp()
+    items: list = [2, 3, 4]
 
-        for x in items:
-          link.insert_at_front(x)
+    for x in items:
+      link.insert_at_front(x)
 
-        lst: list = link.return_list()
-        self.assertEqual(len(lst), len(items))
-        self.assertEqual(lst, items[::-1])
+    lst: list = link.to_list()
+    self.assertEqual(len(lst), len(items))
+    self.assertEqual(lst, items[::-1])
 
-    def test_fn_insert_at_rear(self):
-        link: LinkList = self.setUp()
-        items: list = [2, 3, 4]
+  def test_fn_insert_at_rear(self):
+    link: LinkList = self.setUp()
+    items: list = [2, 3, 4]
 
-        for x in items:
-          link.insert_at_rear(x)
+    for x in items:
+      link.insert_at_rear(x)
 
-        lst: list = link.return_list()
-        self.assertEqual(len(lst), len(items))
-        self.assertEqual(lst, items)
+    lst: list = LinkList.to_list(link) # test class method
+    self.assertEqual(len(lst), len(items))
+    self.assertEqual(lst, items)
+
+  def test_fn_mth_from_last(self):
+    link: LinkList = self.setUp()
+    items: list = [1, 2, 3, 4, 5, 6]
+
+    for x in items:
+      link.insert_at_rear(x)
+
+    self.assertEqual(link.find_mth_from_last_element(3), 4)
+    self.assertEqual(link.find_mth_from_last_element(2), 5)
+    self.assertEqual(link.find_mth_from_last_element(6), 1)
+    self.assertEqual(link.find_mth_from_last_element(7), None)
+
+  def test_fn_recursive_linklist(self):
+    link: LinkList = self.setUp()
+    items: list = [1, 2, 3, 4, 5, 6]
+
+    for x in items:
+      link.insert_at_front(x)
+
+    hd = LinkList.reverse_recursive_linklist(link.head)
+    lst = []
+    while(hd is not None):
+      lst.append(hd.data)
+      hd = hd.next
+
+    self.assertEqual(len(lst), len(items))
+    self.assertEqual(lst, items)
+
+  def test_fn_iterative_linklist(self):
+    link: LinkList = self.setUp()
+    items: list = [1, 2, 3, 4, 5, 6]
+
+    for x in items:
+      link.insert_at_front(x)
+
+    link.reverse_iterative_linklist()
+    lst: list = link.to_list()
+    self.assertEqual(len(lst), len(items))
+    self.assertEqual(lst, items)
 
 if __name__ == '__main__':
-    unittest.main()
+  unittest.main()
